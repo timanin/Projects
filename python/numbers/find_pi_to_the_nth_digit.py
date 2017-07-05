e69d71f71db9935fd3b217435cd72e932672d841
Sergey Timanin <timanin@gmail.com>
2017-07-05 20:07:39 +0200

    Add the first Numbers exercise

---
+++ python/numbers/find_pi_to_the_nth_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Enter a number and have the program generate PI up to that many
+decimal places. Keep a limit to how far the program will go.
+"""
+
+import sys
+import math
+
+precision = 5 #sys.argv[1]
+
+print('pi: {:.$precisionf}'.format(math.pi))

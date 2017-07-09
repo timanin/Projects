ad2f7ca2c6fa14a42594a64914a19358dc5b7ffa
Sergey Timanin <timanin@gmail.com>
2017-07-09 23:04:04 +0200

    Refactor further

--- python/threading/progressbar_3.py
+++ python/threading/progressbar_3.py
@@ -4,22 +4,7 @@
 download in progress. The progress bar will be on a separate thread
 and will communicate with the main thread using delegates."""
 
-import threading
-from time import sleep
 import requests
-
-
-downloaded = 0
-
-
-def download(url, filename):
-    """Downloads the specified URL into filename."""
-    global downloaded
-    req = requests.get(url, stream=True)
-    with open(filename, 'wb') as file:
-        for chunk in req.iter_content(chunk_size=128):
-            file.write(chunk)
-            downloaded += 128
 
 
 def get_size(url):
@@ -28,51 +13,49 @@ def get_size(url):
     return int(req.headers['Content-Length'])
 
 
-def check_progress(url):
-    """Prints the download progress."""
-    size = get_size(url)
-
-    done = 0
-    while done < size:
-        done = downloaded
-        progress = int(done * 100 / size)
-        hashes = '#' * int(progress / 2)
-        print('Progress: {:s} ({:d}%)'.format(hashes, progress),
-              end='\r', flush=True)
-        sleep(1)
-    print('')
+def download(url, filename):
+    """Downloads the specified URL into filename."""
+    downloaded = 0
+    file_size = get_size(url)
+    req = requests.get(url, stream=True)
+    with open(filename, 'wb') as file:
+        for chunk in req.iter_content(chunk_size=1024):
+            file.write(chunk)
+            downloaded += 1024
+            percent_downloaded = int(100 * downloaded / file_size)
+            yield percent_downloaded
 
 
-def download_with_progress(url, filename):
-    """
-    Runs two threads: one to download and another to observe and print
-    out the progress.
-    """
-
-    threads = []
-    thread1 = threading.Thread(target=download, args=(url, filename))
-    threads.append(thread1)
-    thread2 = threading.Thread(target=check_progress, args=(url,))
-    threads.append(thread2)
-
-    thread1.start()
-    thread2.start()
+def print_progress(percent):
+    """Prints progress to stdout."""
+    hashes = '#' * int(percent / 2)
+    print('Progress: {:s} ({:d}%)'.format(hashes, percent),
+          end='\r', flush=True)
+    if percent == 100:
+        print('')
 
 
-def main():
-    """main function"""
-
-    domain_name = 'distfiles.gentoo.org'
-    query_path = '/releases/amd64/autobuilds/20170629/'
-    filename = 'install-amd64-minimal-20170629.iso'
-    url = 'http://{d}/{q}/{f}'.format(
+def make_url(domain_name, query_path, filename):
+    """Return a full URL."""
+    return 'https://{d}/{q}/{f}'.format(
         d=domain_name,
         q=query_path,
         f=filename
     )
 
-    download_with_progress(url, filename)
 
+def main():
+    """main function"""
+
+    filename = 'Skype_7.56.776.dmg'
+    url = make_url(
+        'download.skype.com',
+        'macosx/26d34dde05a09c44a8a29fa28eb15940',
+        filename
+    )
+
+    for i in download(url, filename):
+        print_progress(i)
 
 if __name__ == '__main__':
     main()
